07bc2a7dd3fc0309d7e309da11436b23dfc9019a
Godmar Back <godmar@gmail.com>
2015-02-02 14:02:43 -0500

    first set of edits

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 i_scirptdir=$(cd "$(dirname "$0")"; pwd)
 $i_scirptdir/gnuplot/plot.sh
 pdflatex -shell-escape paper
-pdflatex -shell-escape paper
 bibtex paper
+pdflatex -shell-escape paper
 pdflatex -shell-escape paper
