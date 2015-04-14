45937847b8add180d260c99c6a81496a00346f1f
Blade Pan <blade@msn.cn>
2015-04-14 00:29:01 -0400

    background

--- codeexamples/async_auto.js
+++ codeexamples/async_auto.js
@@ -1,1 +1,27 @@
+var fs = require('fs');
 var async = require('async');
+fs.readFile('/etc/passwd', function(err, data){
+    if (err) throw err;
+    var file1Length = data.length;
+    fs.readFile('/etc/hosts', function(err, data){
+        if (err) throw err;
+        console.log("Total file length is " + (file1Length + data.length));
+    });
+});
+var readFileLength = function(fileName){
+    return function(next){
+        fs.readFile(fileName, function(err, data){
+            if (err) return next(err);
+            next(null, data.length);
+        });        
+    };
+};
+async.auto({
+    'file1' : readFileLength('/etc/passwd'),
+    'file2' : readFileLength('/etc/hosts'),
+    'print' : ['file1', 'file2', function(next, results){
+        console.log("Total file length is " + (results.file1 + results.file2)); 
+    }]
+}, function(err){
+    if (err) throw err;
+});
---
+++ codeexamples/async_each.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var async = require('async');
+var files = ['/etc/passwd', '/etc/hosts', '/etc/profile'];
+var finished = 0, totalSize = 0, totalSize2 = 0;
+for (var i = 0; i < files.length; i++) {
+    fs.readFile(files[i], function(err, data){
+        totalSize += data.length;
+        finished++;
+        if (finished == files.length) { 
+            console.log("total file size "+ totalSize);
+        };
+    });
+};
+async.each(files, function(fileName, next){
+    fs.readFile(fileName, function(err, data){
+        totalSize2 += data.length;
+        next();
+    });
+}, function(err){
+    console.log("total file size "+ totalSize2);
+});
